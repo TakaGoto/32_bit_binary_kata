c19fc97432656d95162ad48d47ca8ac25247e89d
Takayuki Goto <tak.yuki@gmail.com>
2013-07-25 11:55:14 -0500

    init

--- lib/binary32.rb
+++ lib/binary32.rb
@@ -1,48 +1,27 @@
 class Binary32
-  def self.get_sign(number)
-    return '1' if number < 0
+  ExponentBias = 127
+
+  def self.convert(num)
+    @num = num
+    sign + exponent + mantissa
+  end
+
+  def self.sign
+    return '1' if @num < 0
     '0'
   end
 
-  def self.integer_to_binary(number)
-    number.floor.to_s(2)
+  def self.exponent
+    integer_binary = @num.floor.to_s(2).to_i
+    exponent = Math.log10(integer_binary.abs).floor
+    exponent = (ExponentBias + exponent).to_s(2)
+    exponent.insert(0, '0') if exponent.size == 7
+    exponent
   end
 
-  def self.fraction_to_binary(number)
-    precision_limit = 23 - integer_to_binary(number).to_i.size
-    number -= number.floor
-    binary_fraction = '.'
-
-    until((number % 1).zero? || binary_fraction.size == precision_limit)
-      number *= 2
-      binary_fraction += number.floor.to_s
-      number -= number.floor
-    end
-
-    binary_fraction
-  end
-
-  def self.float_number_to_binary(number)
-    integer_to_binary(number) + fraction_to_binary(number)
-  end
-
-  def self.normalized_exponent(binary)
-    Math.log10(binary.to_f).floor
-  end
-
-  def self.mantissa(binary)
-    binary[0] = ''
-    binary.gsub!(/\./, '')
-    mantissa = binary + ('0' * (23 - binary.length))
+  def self.mantissa
+    mantissa = (@num.abs * 2 ** 23).floor.to_s(2)[0..23]
+    mantissa[0] = ''
     mantissa
-  end
-
-  def self.exponent_to_binary(number)
-    (number + 127).to_s(2)
-  end
-
-  def self.convert_float_to_binary(number)
-    binary_fraction = float_number_to_binary(number)
-    get_sign(number) + '-' + exponent_to_binary(normalized_exponent(binary_fraction)) + '-' + mantissa(binary_fraction)
   end
 end
--- spec/binary32_spec.rb
+++ spec/binary32_spec.rb
@@ -1,104 +1,43 @@
 require 'binary32'
 
+def get_binary(num)
+  "%032b" % [num].pack('e').reverse.each_char.inject(0) { |sum,c| sum = (sum << 8) + c.ord }
+end
+
 describe Binary32 do
-  describe "#get_sign" do
-    it 'gets the binary sign' do
-      described_class.get_sign(12.375).should == '0'
-    end
 
-    it 'gets the binary sign for a negative number' do
-      described_class.get_sign(-12.375).should == '1'
-    end
+  it 'returns 32 bit binary for 0.0' do
+    binary = get_binary(0.0)
+    described_class.should_receive(:sign).and_return(binary[0])
+    described_class.should_receive(:exponent).and_return(binary[1..8])
+    described_class.should_receive(:mantissa).and_return(binary[9..31])
+    described_class.convert(0.0).should == binary
   end
 
-  describe "#integer_to_binary" do
-    it 'converts 12.375 to 1100' do
-      described_class.integer_to_binary(12.375).should == '1100'
-    end
+  describe "#mantissa" do
+    {
+      12.345 => get_binary(12.345),
+      1.9999999 => get_binary(1.9999999),
+      -2.5521175E38 => get_binary(-2.5521175E38),
+      Math::PI => get_binary(Math::PI)
+    }.each do |num, binary|
+      it "returns mantissa for #{num}" do
+        described_class.should_receive(:sign).and_return(binary[0])
+        described_class.should_receive(:exponent).and_return(binary[1..8])
+        (described_class.convert(num).to_i - binary.to_i).abs.should <= 1
+      end
 
-    it 'converts 11.375 to 1011' do
-      described_class.integer_to_binary(11.375).should == '1011'
-    end
+      it "returns exponent for #{num}" do
+        described_class.should_receive(:sign).and_return(binary[0])
+        described_class.should_receive(:mantissa).and_return(binary[9..31])
+        described_class.convert(num).should == binary
+      end
 
-    it 'converts 123.456 to 1111011' do
-      described_class.integer_to_binary(123.456).should == '1111011'
-    end
-  end
-
-  describe "#fraction_to_binary" do
-    it 'converts 12.375 to .011' do
-      described_class.fraction_to_binary(12.375).should == '.011'
-    end
-
-    it 'converts 12.25 to .01' do
-      described_class.fraction_to_binary(12.25).should == '.01'
-    end
-  end
-
-  describe "#float_number_to_binary" do
-    it 'converts 12.375 to 1100.011' do
-      described_class.float_number_to_binary(12.375).should == '1100.011'
-    end
-
-    it 'converts 12.25 to 1100.01' do
-      described_class.float_number_to_binary(12.25).should == '1100.01'
-    end
-
-    xit 'converts 1.9999999 to' do
-      described_class.float_number_to_binary(1.9999999).should == '1100.01'
-    end
-  end
-
-  describe "normalized_exponent" do
-    it 'gets exponent 3 for 1100.011' do
-      described_class.normalized_exponent('1100.011').should == 3
-    end
-
-    it 'gets exponent 4 for 11000.11' do
-      described_class.normalized_exponent('11000.11').should == 4
-    end
-  end
-
-  describe "mantissa" do
-    it 'converts 1100.011 to its mantissa' do
-      described_class.mantissa('1100.011').should == '10001100000000000000000'
-    end
-  end
-
-  describe "#exponent_to_binary" do
-    it 'gets the binary for the exponent 3' do
-      described_class.exponent_to_binary(3).should == '10000010'
-    end
-
-    it 'gets the binary for the exponent 4' do
-      described_class.exponent_to_binary(4).should == '10000011'
-    end
-  end
-
-  describe "#convert_float_to_binary" do
-    it 'converts 12.375 to its 32 bit binary' do
-      described_class.convert_float_to_binary(12.375).should ==
-        '0-10000010-10001100000000000000000'
-    end
-
-    it 'converts 12.25 to its 32 bit binary' do
-      described_class.convert_float_to_binary(12.25).should ==
-        '0-10000010-10001000000000000000000'
-    end
-
-    it 'converts 123.456 to its 32 bit binary' do
-      described_class.convert_float_to_binary(123.456).should ==
-        '0-10000101-11101101110100101111000'
-    end
-
-    xit 'converts 1.9999999 to its 32 bit binary' do
-      described_class.convert_float_to_binary(1.9999999).should ==
-        '0-01111111-11111111111111111111111'
-    end
-
-    it 'converst 0.0 to its 32 bit binary' do
-      described_class.convert_float_to_binary(0.0).should ==
-        '0-0000000000000000000000000000000'
+      it "returns sign for #{num}" do
+        described_class.should_receive(:exponent).and_return(binary[1..8])
+        described_class.should_receive(:mantissa).and_return(binary[9..31])
+        described_class.convert(num).should == binary
+      end
     end
   end
 end
